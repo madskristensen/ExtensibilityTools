c99bb7dc72c9d7ccb61484376dc863b25a23d0bd
Mads Kristensen <post@madskristensen.net>
2015-04-07 17:53:38 -0700

    Added brace completion

--- README.md
+++ README.md
@@ -5,7 +5,10 @@
 Download this extension from the [VS Gallery](https://visualstudiogallery.msdn.microsoft.com/cd0b1938-4513-4e57-b9b7-c674b4a20e79)
 or get the [nightly build](http://vsixgallery.com/extension/f8330d54-0469-43a7-8fc0-7f19febeb897/).
 
-### Features
+### Pkgdef files
 
-#### Pkgdef syntax highlighting
+#### Syntax highlighting
 Colorizes registry keys, strings, keywords, comments and more.
+
+#### Brace matching
+Matches parantheses and square brackets.
---
+++ src/Pkgdef/Tagger/BraceMatchingTaggerProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+
+namespace MadsKristensen.ExtensibilityTools.Pkgdef
+{
+    [Export(typeof(IViewTaggerProvider))]
+    [ContentType(PkgdefContentTypeDefinition.PkgdefContentType)]
+    [TagType(typeof(TextMarkerTag))]
+    class BraceMatchingTaggerProvider : IViewTaggerProvider
+    {
+        public ITagger<T> CreateTagger<T>(ITextView textView, ITextBuffer buffer) where T : ITag
+        {
+            if (textView == null)
+                return null;
+
+            //provide highlighting only on the top-level buffer
+            if (textView.TextBuffer != buffer)
+                return null;
+
+            return new BraceMatchingTagger(textView, buffer) as ITagger<T>;
+        }
+    }
+}

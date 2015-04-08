07462717cd2a02ccba0db69fd3d1ad772243c257
Mads Kristensen <post@madskristensen.net>
2015-04-07 22:26:57 -0700

    Refactorings

--- src/Pkgdef/Tagger/OutliningTagger.cs
+++ src/Pkgdef/Tagger/OutliningTagger.cs
@@ -1,31 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.Composition;
 using System.Linq;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Tagging;
-using Microsoft.VisualStudio.Utilities;
 
 namespace MadsKristensen.ExtensibilityTools.Pkgdef
 {
-    [Export(typeof(ITaggerProvider))]
-    [TagType(typeof(IOutliningRegionTag))]
-    [ContentType(PkgdefContentTypeDefinition.PkgdefContentType)]
-    internal sealed class OutliningTagger : ITaggerProvider
-    {
-        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
-        {
-            return buffer.Properties.GetOrCreateSingletonProperty(() => new RegionTagger(buffer)) as ITagger<T>;
-        }
-    }
-
-    internal sealed class RegionTagger : ITagger<IOutliningRegionTag>
+    internal sealed class OutliningTagger : ITagger<IOutliningRegionTag>
     {
         ITextBuffer buffer;
         ITextSnapshot snapshot;
         IEnumerable<Region> regions;
 
-        public RegionTagger(ITextBuffer buffer)
+        public OutliningTagger(ITextBuffer buffer)
         {
             this.buffer = buffer;
             this.snapshot = buffer.CurrentSnapshot;
---
+++ src/Pkgdef/Tagger/OutliningTaggerProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+
+namespace MadsKristensen.ExtensibilityTools.Pkgdef
+{
+    [Export(typeof(ITaggerProvider))]
+    [TagType(typeof(IOutliningRegionTag))]
+    [ContentType(PkgdefContentTypeDefinition.PkgdefContentType)]
+    internal sealed class OutliningTaggerProvider : ITaggerProvider
+    {
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+            return buffer.Properties.GetOrCreateSingletonProperty(() => new OutliningTagger(buffer)) as ITagger<T>;
+        }
+    }
+}

e4923f5322153ad3abb549ec6281e82221c5a538
Pawel Hofman <pawel@codetitans.pl>
2015-04-17 23:09:53 +0200

    Fixed keyword detection regex

--- src/Pkgdef/Variables.cs
+++ src/Pkgdef/Variables.cs
@@ -8,7 +8,7 @@ namespace MadsKristensen.ExtensibilityTools.Pkgdef
         public static Regex RegistryPath = new Regex(@"(\[)(?<path>[^\]]+)?(\])?", RegexOptions.Compiled);
         public static Regex String = new Regex(@"""(?<content>[^""]+)?""?", RegexOptions.Compiled);
         public static Regex EntryKey = new Regex(@"^([\s]+)?(?<dword>(@)|("")([^""]+)(""))(?<operator>([\s]+)?=)", RegexOptions.Compiled);
-        public static Regex Keyword = new Regex(@"\$([^\$]+)\$|(?(?<==)([\s]+)?(dword|hex)(?=:))", RegexOptions.Compiled);
+        public static Regex Keyword = new Regex(@"\$([^\$]+)\$|(?=(?<==)([\s]+)?(dword|hex)(?=:))", RegexOptions.Compiled);
         public static Regex Guid = new Regex(@"{(?<content>[^}]+)}", RegexOptions.IgnoreCase | RegexOptions.Compiled);
     }
 }

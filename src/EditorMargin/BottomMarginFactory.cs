6b25309ea65ac32971b90da9172082c2ae698622
Mads Kristensen <post@madskristensen.net>
2016-08-24 08:39:52 -0700

    Added missing null check Fixed #41

--- src/EditorMargin/BottomMarginFactory.cs
+++ src/EditorMargin/BottomMarginFactory.cs
@@ -22,7 +22,7 @@ namespace MadsKristensen.ExtensibilityTools.EditorMargin
 
         public IWpfTextViewMargin CreateMargin(IWpfTextViewHost wpfTextViewHost, IWpfTextViewMargin marginContainer)
         {
-            if (!ExtensibilityToolsPackage.Options.ShowBottomMargin)
+            if (ExtensibilityToolsPackage.Options == null || !ExtensibilityToolsPackage.Options.ShowBottomMargin)
                 return null;
 
             return new BottomMargin(wpfTextViewHost.TextView, _classifierService, _documentService);
